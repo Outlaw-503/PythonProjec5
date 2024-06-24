c1f352875e5e4efaff5ad1d45a596406f9fb0339
Outlaw-503 <115690155+Outlaw-503@users.noreply.github.com>
2024-06-24 12:18:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ with s:
   st.bar_chart(data=data,x="brand_name",y="price")
   st.write("A Chart showing total sales according to processor brands \n")
   st.bar_chart(data=data,x="processor_brand",y="price")
-  st.write("A Chart showing total sales according to if extended memory is available or not \n")
-  st.scatter_chart(data=data,x="extended_memory_available",y="price")
+  st.write("A Chart showing refresh rates of smartphones in terms of their operating system \n")
+  st.bar_chart(data=data,x="os",y="refresh_rate")
