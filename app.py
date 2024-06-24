1794d8119b8b4f0446ed30d440916a1e9678d6bc
Outlaw-503 <115690155+Outlaw-503@users.noreply.github.com>
2024-06-24 12:06:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-
+import pandas as pd
+import numpy as np
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+s=st.container()
+with s:
+  data=pd.read_csv("smartphones_cleaned_v6.csv")
+  data.dropna(subset=["rating","processor_brand","num_cores","processor_speed","battery_capacity","num_front_cameras","os","primary_camera_front"],axis=0,inplace=True)
+  data.drop(columns=["fast_charging","extended_upto"],inplace=True)
+  st.bar_chart(data=data,x="brand_name",y="price")
+  
